6459dae1681a96d0b21745927e76fd76173af085
Josef Ivan Monje <j.monje@klab.com>
2017-02-01 14:49:56 +0800

    initial commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt-get install --no-install-recommends xinit matchbox xwit xserver-xorg xserver-xorg-video-fbdev x11-xserver-utils ttf-mscorefonts-installer sqlite3 libnss3 unclutter chromium-browser -y
+sudo apt-get update -y
+sudo apt-get dist-upgrade -y
+
+sudo mv -f rc.local /etc/rc.local
+sudo mv -f xinitrc ~/.xinitrc
+
+echo '\nstartx\n' >> ~/.bashrc
+
+sudo reboot
