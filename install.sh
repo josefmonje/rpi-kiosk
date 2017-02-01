61676e227cb9d5d26d33473075eb5f5ce403c4d6
Josef Ivan Monje <j.monje@klab.com>
2017-02-01 15:47:34 +0800

    include from which image name

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+# FROM jessie lite
 sudo apt-get install --no-install-recommends xinit matchbox xwit xserver-xorg xserver-xorg-video-fbdev x11-xserver-utils ttf-mscorefonts-installer sqlite3 libnss3 unclutter chromium-browser -y
 sudo apt-get update -y
 sudo apt-get dist-upgrade -y
