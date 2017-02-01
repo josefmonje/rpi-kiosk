cdb7c458dc5372d6a331c21b661c8760154fd97b
Josef Ivan Monje <j.monje@klab.com>
2017-02-01 22:22:40 +0800

    vertical list

--- install.sh
+++ install.sh
@@ -1,12 +1,29 @@
 # FROM jessie lite
-sudo apt-get install --no-install-recommends xinit matchbox xwit xserver-xorg xserver-xorg-video-fbdev x11-xserver-utils ttf-mscorefonts-installer sqlite3 libnss3 unclutter chromium-browser -y
-sudo apt-get update -y
-sudo apt-get dist-upgrade -y
-
 sudo cat rc.local >> /etc/rc.local
 sudo mv -f xinitrc /etc/xinitrc
-sudo ln -sf /etc/xinitrc ~/.xinitrc
-
+sudo ln -sf /etcx/initrx ~/.xinitrc
 echo '\nstartx\n' >> ~/.bashrc
+
+sudo apt-get install --no-install-recommends \
+    xinit \
+    matchbox \
+    xwit \
+    xserver-xorg \
+    xserver-xorg-video-fbdev \
+    x11-xserver-utils \
+    ttf-mscorefonts-installer \
+    sqlite3 \
+    libnss3 \
+    unclutter \
+    chromium-browser \
+
+    mosh \
+    tmux \
+    w3m-img \
+    mc \
+    moc \
+    -y
+sudo apt-get update -y
+sudo apt-get dist-upgrade -y
 
 sudo reboot
